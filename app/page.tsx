4be2ea9697f5eac4902f83843336d69f81828a3a
Magdalena Gozdalik <magdagozdalik28@gmail.com>
2025-03-09 23:37:32 +0000

    Add custom font

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,20 @@
+import { Button } from '@/components/ui/button';
+
 export default function Home() {
   return (
     <main className='flex h-full flex-col items-center justify-center bg-radial from-sky-400 to-blue-800'>
-      <div className='space-y-6'>
-        <h1 className='text-6xl font-semibold text-white drop-shadow-md'>
+      <div className='space-y-6 text-center'>
+        <h1 className='text-6xl font-semibold text-white drop-shadow-md font-primary'>
           🔐 Auth
         </h1>
         <p className='text-white text-center text-lg'>
           A simple authentication service
         </p>
+        <div>
+          <Button variant='secondary' size='lg'>
+            Sign in
+          </Button>
+        </div>
       </div>
     </main>
   );
