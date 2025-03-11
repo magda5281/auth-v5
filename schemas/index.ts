9947c7a073e63b4a153a1eb6b25d86257ea6d7d2
Magdalena Gozdalik <magdagozdalik28@gmail.com>
2025-03-11 11:26:05 +0000

    Add register form

--- schemas/index.ts
+++ schemas/index.ts
@@ -7,3 +7,13 @@ export const LoginSchema = z.object({
     message: 'Password is required',
   }),
 });
+
+export const RegisterSchema = z.object({
+  email: z
+    .string({ invalid_type_error: 'Must be a string' })
+    .email({ message: 'Must be a valid email' }),
+  password: z.string().min(6, {
+    message: 'Minimum 6 characters',
+  }),
+  name: z.string().min(1, { message: 'Name is required' }),
+});
