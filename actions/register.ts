52df77c71a7e7e4e4ed2f18321d2be14f30a0d4c
Magdalena Gozdalik <magdagozdalik28@gmail.com>
2025-03-12 19:41:13 +0000

    Create database user query functions

--- actions/register.ts
+++ actions/register.ts
@@ -1,9 +1,9 @@
 'use server';
-
-import { RegisterSchema } from '@/schemas';
 import { z } from 'zod';
 import * as bcrypt from 'bcryptjs';
 import { db } from '@/lib/db';
+import { RegisterSchema } from '@/schemas';
+import { getUserByEmail } from '@/data/user';
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
   const validatedFields = RegisterSchema.safeParse(values);
@@ -13,9 +13,7 @@ export const register = async (values: z.infer<typeof RegisterSchema>) => {
 
   const { email, password, name } = validatedFields.data;
   const hashedPassword = await bcrypt.hash(password, 10);
-  const existingUser = await db.user.findUnique({
-    where: { email },
-  });
+  const existingUser = await getUserByEmail(email);
 
   if (existingUser) {
     return { error: 'Email already in use' };
