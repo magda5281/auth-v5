07211e256f4f084d9f0d29b3961fbf4bb4ae5077
Magdalena Gozdalik <magdagozdalik28@gmail.com>
2025-03-12 19:43:12 +0000

    Add middleware

---
+++ routes.ts
@@ -0,0 +1,26 @@
+/**
+ * An array of routes that are accessible to the public
+ * These routes do not require authentication
+ * @type {string[]}
+ */
+
+export const publicRoutes = ['/'];
+/**
+ * An array of routes that are used for authentication
+ * These routes will redirect logged users to /settings
+ * @type {string[]}
+ */
+export const authRoutes = ['/auth/login', '/auth/register'];
+
+/**
+ * The prefix for API authentication routes
+ * Routes that atart with this prefix are used for API authentication purposes
+ * @type {string}
+ */
+export const apiAuthPrefix = '/api/auth';
+
+/**
+ * the default redirect route after login
+ * @type {string}
+ */
+export const DEFAULT_LOGIN_REDIRECT = '/settings';
