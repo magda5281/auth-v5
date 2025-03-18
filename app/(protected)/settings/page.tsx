da9e9ad395b0dc9771e8cc7f9884be6a851acc0b
Magdalena Gozdalik <magdagozdalik28@gmail.com>
2025-03-18 18:22:00 +0000

    Add SessionProvider

--- app/(protected)/settings/page.tsx
+++ app/(protected)/settings/page.tsx
@@ -1,17 +1,18 @@
-import { auth, signOut } from '@/auth';
+'use client';
 import { Button } from '@/components/ui/button';
+import { useSession } from 'next-auth/react';
 
-const page = async () => {
-  const session = await auth();
+const page = () => {
+  const session = useSession();
 
   return (
     <div>
       {JSON.stringify(session)}
       <form
-        action={async () => {
-          'use server';
-          await signOut({ redirectTo: '/', redirect: true });
-        }}
+      // action={async () => {
+      //   'use server';
+      //   await signOut({ redirectTo: '/', redirect: true });
+      // }}
       >
         <Button type='submit'>Sign out</Button>
       </form>
