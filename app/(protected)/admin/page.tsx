4c6a8c22958c1ea8610972a5b7859c4bf3d89a7b
Magdalena Gozdalik <magdagozdalik28@gmail.com>
2025-03-26 15:10:09 +0000

    Add admin page based on user role permission

--- app/(protected)/admin/page.tsx
+++ app/(protected)/admin/page.tsx
@@ -1,8 +1,22 @@
-import { currentRole } from '@/lib/auth';
+'use client';
+import { RoleGate } from '@/components/auth/role-gate';
+import { FormSuccess } from '@/components/form-success';
+import { Card, CardContent, CardHeader } from '@/components/ui/card';
+import { UserRole } from '@prisma/client';
 
 const AdminPage = () => {
-  const role = currentRole();
-  return <div>Current role: {role}</div>;
+  return (
+    <Card className='w-full max-w-[600px]'>
+      <CardHeader>
+        <p className='text-2xl font-semibold text-center'>🔑 Admin</p>
+      </CardHeader>
+      <CardContent className='space-y-4'>
+        <RoleGate allowedRole={UserRole.ADMIN}>
+          <FormSuccess message='🎉 You are allowed to see this content!' />
+        </RoleGate>
+      </CardContent>
+    </Card>
+  );
 };
 
 export default AdminPage;
---
+++ components/auth/role-gate.tsx
@@ -0,0 +1,18 @@
+'use client';
+import { useCurrentRole } from '@/hooks/useCurrentRole';
+import { UserRole } from '@prisma/client';
+import { FormError } from '@/components/form-error';
+
+interface RoleGateProps {
+  children: React.ReactNode;
+  allowedRole: UserRole;
+}
+export const RoleGate = ({ children, allowedRole }: RoleGateProps) => {
+  const role = useCurrentRole();
+  if (role !== allowedRole) {
+    return (
+      <FormError message='You do not have permission to see that content ' />
+    );
+  }
+  return <>{children}</>;
+};
