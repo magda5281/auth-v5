1c437d1fee503bf248653b74cb1fa5135fcaa3a9
Magdalena Gozdalik <magdagozdalik28@gmail.com>
2025-04-26 18:41:18 +0100

    Font fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,26 +1,16 @@
 import type { Metadata } from 'next';
-// import { Geist, Geist_Mono, Poppins } from 'next/font/google';
+import { Poppins } from 'next/font/google';
 import './globals.css';
 import { auth } from '@/auth';
 import { SessionProvider } from 'next-auth/react';
 import { Toaster } from '@/components/ui/sonner';
 
 // // Load Poppins font
-// const poppins = Poppins({
-//   variable: '--font-poppins',
-//   subsets: ['latin'],
-//   weight: ['400', '600', '700'], // Choose font weights you need
-// });
-
-// const geistSans = Geist({
-//   variable: '--font-geist-sans',
-//   subsets: ['latin'],
-// });
-
-// const geistMono = Geist_Mono({
-//   variable: '--font-geist-mono',
-//   subsets: ['latin'],
-// });
+const poppins = Poppins({
+  variable: '--font-poppins',
+  subsets: ['latin'],
+  weight: ['400', '600', '700'], // Choose font weights you need
+});
 
 export const metadata: Metadata = {
   title: 'Next App OAuth',
