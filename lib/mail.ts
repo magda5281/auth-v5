6c2d50193028885e530687143fae74d277504d98
Magdalena Gozdalik <magdagozdalik28@gmail.com>
2025-04-27 12:13:26 +0100

    Update resend email

--- lib/mail.ts
+++ lib/mail.ts
@@ -2,9 +2,10 @@ import { Resend } from 'resend';
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 const domain = process.env.NEXT_PUBLIC_APP_URL;
+const fromEmail = process.env.NEXT_PUBLIC_APP_EMAIL || 'onboarding@resend.dev';
 export const sendTwoFactorTokenEmail = async (email: string, token: string) => {
   await resend.emails.send({
-    from: 'mail@herdevlab.co.uk',
+    from: fromEmail,
     to: email,
     subject: '2FA code',
     html: `<p>Your 2FA code :${token}</p>`,
@@ -15,7 +16,7 @@ export const sendPasswordResetEmail = async (email: string, token: string) => {
   const resetLink = `${domain}/auth/new-password?token=${token}`;
 
   await resend.emails.send({
-    from: 'mail@herdevlab.co.uk',
+    from: fromEmail,
     to: email,
     subject: 'Reset your password',
     html: `<p>Click <a href="${resetLink}">here</a> to reset your password. </p>`,
@@ -26,7 +27,7 @@ export const sendVerificationEmail = async (email: string, token: string) => {
   const confirmLink = `${domain}/auth/new-verification?token=${token}`;
 
   await resend.emails.send({
-    from: 'mail@herdevlab.co.uk',
+    from: fromEmail,
     to: email,
     subject: 'Confirm your email',
     html: `<p>Click <a href="${confirmLink}">here</a> to confirm email. </p>`,
